eacfaee36de0aaddc5b7233f0d7f189424e361d1
Jose A Dianes <jadianes@gmail.com>
2011-10-23 10:43:29 +0100

    Fixing about controller action problem

--- about.php
+++ about.php
@@ -1,11 +1,12 @@
 <?php
-	require('Smarty.class.php');
+	require_once('models/config.php');
+	require_once('Smarty.class.php');
 	$smarty = new Smarty();
 	
 	// assign some content. This would typically come from
 	// a database or other source, but we'll use static
 	// values for the purpose of this example.
-	$smarty->assign('title', 'WinesAlike');
+	$smarty->assign('sitename', WA_WEBSITE_NAME);
 	$smarty->assign('slogan', 'Trust your taste');
 
 	// display it
