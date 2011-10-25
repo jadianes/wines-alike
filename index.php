fe1f6ceb094b2b2266d850259e05b25116a1acd8
Jose A Dianes <jadianes@gmail.com>
2011-10-25 14:46:13 +0100

    Introducing OO controllers

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once('controllers/front.php');
 require_once('controllers/icontroller.php');
-require_once('controllers/index.php');
+require_once('controllers/ratings.php');
 
 // Initialize the FrontController
 $front = FrontController::getInstance();
