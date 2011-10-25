87b8f603d1c6d3d9dc187b279e8f92bb713e7cc8
Jose A Dianes <jadianes@gmail.com>
2011-10-25 12:51:15 +0100

    Introducing OO controllers

--- controllers/index.php
+++ controllers/index.php
@@ -1,8 +1,9 @@
 <?php
 
-require_once ( dirname(__FILE__).'/../models/config.php');
-
-
+require_once(dirname(__FILE__).'/../models/config.php');
+require_once(dirname(__FILE__).'/../models/user_manager_class.php'); 
+require_once(dirname(__FILE__).'/../models/ratings_class.php');
+require_once(dirname(__FILE__).'/../template_fns.php');
 /**
  * Index controller
  *
@@ -20,10 +21,39 @@ class IndexController implements IController
 	 **/
 	public function index()
 	{
+		$user_manager = new UserManager(); // this class start session in its constructor
 		$smarty = new Smarty_WinesAlike();
-		$smarty->assign( 'sitename', WA_WEBSITE_NAME );
-		$smarty->display( 'test.tpl' );
+
+		//create short variable names
+		$email = $_POST['email'];
+		$passwd = $_POST['passwd'];
+
+		if ($email && $passwd)
+		// they have just tried logging in
+		{
+		  try
+		  {
+		    $user_manager->login_exists($email, $passwd);  
+		    // if they are in the database register the user id
+		    $user_manager->register_valid_user($email);
+		  }
+		  catch(Exception $e) {
+			$smarty->assign('sitename', WA_WEBSITE_NAME);
+			$smarty->assign('slogan', 'Trust your taste');
+			$smarty->assign('message', 'Sorry, it was impossible to log you in. '+$e->getMessage());
+			$smarty->display('system_message.tpl');
+		    exit;
+		  }      
+		}
+
+		if ($user_manager->check_valid_user()) {
+			display_latest_ratings_member();
+		} else {
+			display_latest_ratings();
+		}
+
 	}
+
 
 } // END class 
 
