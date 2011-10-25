8c57cafb66dc501019f7a254eaf86b2dfea398d3
Jose A Dianes <jadianes@gmail.com>
2011-10-25 14:38:31 +0100

    Introducing OO controllers

--- controllers/front.php
+++ controllers/front.php
@@ -71,7 +71,6 @@ class FrontController
 	 **/
 	public function route()
 	{
-		echo('<p>Controller:'.$this->getController().' Action:'.$this->getAction().'</p>');
 		if ( class_exists( $this->getController() ) ) 
 		{
 			$rc = new ReflectionClass( $this->getController() );
