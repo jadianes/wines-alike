bafd57ba2d8fe4987fba624dca4b4c48d3487881
Jose A Dianes <jadianes@gmail.com>
2011-10-26 16:25:28 +0100

    Introducing OO controllers

--- controllers/user.php
+++ controllers/user.php
@@ -4,6 +4,8 @@ require_once(dirname(__FILE__).'/../models/config.php');
 require_once(dirname(__FILE__).'/../models/user_manager_class.php'); 
 require_once(dirname(__FILE__).'/../models/ratings_class.php');
 require_once(dirname(__FILE__).'/../template_fns.php');
+require_once(dirname(__FILE__).'/../data_valid_fns.php');
+
 /**
  * User menu controller
  *
@@ -28,6 +30,78 @@ class useractions implements IController
 		display_latest_ratings();
 	}
 
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author Jose A Dianes
+	 **/
+	public function change_password()
+	{
+		$user_manager = new UserManager();
+
+		$smarty = new Smarty_WinesAlike();
+
+		// create short variable names
+		$old_passwd = $_POST['old_passwd'];
+		$new_passwd = $_POST['new_passwd'];
+		$new_passwd2 = $_POST['new_passwd2'];
+
+		if ($user_manager->check_valid_user()) {
+			try
+		  	{  
+		    	if (!filled_out($_POST))
+		      		throw new Exception('You have not filled out the form completely. Please try again.');
+		    	if ($new_passwd!=$new_passwd2)
+		       		throw new Exception('Passwords entered were not the same.  Not changed.');
+		    	if (strlen($new_passwd)>30 || strlen($new_passwd)<8)
+		       		throw new Exception('New password must be between 8 and 30 characters. Try again.');
+		  		$email = $user_manager->get_current_user();
+		     	$user_manager->change_password($email,$old_passwd,$new_passwd);
+		    	$user_manager->notify_password($email, $new_passwd);
+		    	$smarty->assign('sitename', WA_WEBSITE_NAME);
+		    	$smarty->assign('slogan', 'Trust your taste');
+				$smarty->assign('message', 'Your new password has been emailed to you.');
+				$smarty->display('member_system_message.tpl');
+		  	}
+		  	catch (Exception $e)
+		  	{
+		    	$smarty->assign('sitename', WA_WEBSITE_NAME);
+		    	$smarty->assign('slogan', 'Trust your taste');
+				$smarty->assign('message', 'Your password could not be changed. '.$e->getMessage());
+				$smarty->display('member_system_message.tpl');
+		  	}
+		} else {
+		  $smarty->assign('sitename', WA_WEBSITE_NAME);
+		  $smarty->assign('slogan', 'Trust your taste');
+		  $smarty->assign('message', 'Ups! You must be logged in to view this page...');
+		  $smarty->display('system_message.tpl');
+		}
+		
+	}
+	
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author Jose A Dianes
+	 **/
+	public function account()
+	{
+		
+	}
+	
+	/**
+	 * undocumented function
+	 *
+	 * @return void
+	 * @author Jose A Dianes
+	 **/
+	public function reports()
+	{
+		
+	}
+	
 } // END class 
 
 ?>
--- templates/user_menu.tpl
+++ templates/user_menu.tpl
@@ -1,9 +1,9 @@
 {* Smarty *}
 
 <ul class="nav secondary-nav">
-		<li><a href="my_account.php">{$username}</a></li>
-		<li><a href='statistics.php'>my taste</a></li>
-		<li><a href='change_passwd_form.php'>change password</a></li>
+		<li><a href='/useractions/account'>{$username}</a></li>
+		<li><a href='/useractions/reports'>my taste</a></li>
+		<li><a href='/useractions/change_password'>change password</a></li>
 		<li><a href='/useractions/logout'>logout</a></li>
 </ul>
 
