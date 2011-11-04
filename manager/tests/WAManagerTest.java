4f153b4e93224afeabea8946af67df8debd423d4
Jose A Dianes <jadianes@gmail.com>
2011-11-04 17:41:16 +0000

    Starting Java Manager Tool

---
+++ manager/tests/WAManagerTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WAManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
